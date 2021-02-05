2794b29ee90c519ded10774b617db44007e0b2bf
gjeffgolden <gjgolden08@gmail.com>
2021-02-05 10:58:53 -0700

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(name, email)
+        })
+        .then(response => response.json())
+        .then(object => {
+        console.log(object)
+        })
+        .catch(error => {
+            alert('Error!')
+    })
+}
+
+// let dogData ={
+//     dogName: 'Byron',
+//     dogBreed: 'Poodle'
+// }
+
+// let configObj = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(dogData)
+// }
+
+// fetch('http://localhost:3000/dogs', configObj)
+//     .then(response => response.json())
+//     .then(object => {
+//         console.log(object)
+//     })
+//     .catch(error => {
+//         alert('Error!')
+//         console.log(error.message)
+//     })
